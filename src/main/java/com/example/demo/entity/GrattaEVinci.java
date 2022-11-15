efec62093d4c06924f6e24a08ed638c999b2a61a
lt-ringm <l.trombetti@ringmaster.it>
2022-11-15 17:48:59 +0100

    Added new entity and repository classes to communicate with the MySQL database.

---
+++ src/main/java/com/example/demo/entity/GrattaEVinci.java
@@ -0,0 +1,41 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class GrattaEVinci {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id; // Identifier
+
+    private Integer number;
+
+    private Boolean is_winner;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Boolean getIs_winner() {
+        return is_winner;
+    }
+
+    public void setIs_winner(Boolean is_winner) {
+        this.is_winner = is_winner;
+    }
+}
